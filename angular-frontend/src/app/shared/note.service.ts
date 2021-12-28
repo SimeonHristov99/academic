2117e721231c315f5006f994502b08fa7cb45794
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-12-28 22:41:23 +0200

    Add notes service.

---
+++ angular-frontend/src/app/shared/note.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Note } from './note.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NoteService {
+
+  notes: Note[]
+
+  constructor() {
+    this.notes = []
+  }
+
+  getNotes() {
+    return this.notes
+  }
+
+  getNote(id:string) {
+    return this.notes.find(n => n.id === id)
+  }
+
+  addNote(note: Note) {
+    this.notes.push(note)
+  }
+
+  updateNote(id: string, updatedFields: Partial<Note>) {
+    const note = this.getNote(id)
+    Object.assign(note, updatedFields)
+  }
+
+  deleteNote(id: string) {
+    const noteIdx = this.notes.findIndex(n => n.id === id)
+    
+    if(noteIdx == -1) return
+    
+    this.notes.splice(noteIdx, 1)
+  }
+}
