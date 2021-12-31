941d0e97d8195554a567b966dd5c1659b7095f53
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-12-31 13:22:02 +0200

    Move PayPal buttons to sidebar.

--- angular-frontend/src/app/sidebar-info/sidebar-info.component.ts
+++ angular-frontend/src/app/sidebar-info/sidebar-info.component.ts
@@ -1,5 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { CartItem } from '../shared/cart.model';
+import { CartService } from '../shared/cart.service';
+
+declare var paypal: any;
 
 @Component({
   selector: 'app-sidebar-info',
@@ -10,15 +13,44 @@ export class SidebarInfoComponent implements OnInit {
 
   @Input() itemsToBuy: CartItem[]
 
-  constructor() {
+  @ViewChild('paypal', { static: true }) paypalElement: ElementRef | undefined
+
+  constructor(
+    private cartService: CartService
+  ) {
     this.itemsToBuy = []
   }
 
   ngOnInit(): void {
+    paypal
+    .Buttons({
+      createOrder: (data: any, actions: { order: { create: (arg0: { purchase_units: { description: string; amount: { currency_code: string; value: number; }; }[]; }) => any; }; }) => {
+        return actions.order.create({
+          purchase_units: [
+            {
+              description: this.itemsToBuy[0].description,
+              amount: {
+                currency_code: 'USD',
+                value: this.itemsToBuy[0].price
+              }
+            }
+          ]
+        })
+      },
+      onApprove: async (data: any, actions: { order: { capture: () => any; }; }) => {
+        const order = await actions.order.capture()
+        this.cartService.deleteItem(this.itemsToBuy[0].id)
+        console.log(order)
+      },
+      onError: (err: any) => {
+        console.log(err)
+      }
+    })
+    .render(this.paypalElement?.nativeElement)
   }
 
-  getSum(items: CartItem[]) {
-    return items.map(s => s.price).reduce((x, a) => x + a, 0)
+  getSum(itemsToBuy: CartItem[]) {
+    return itemsToBuy.map(s => s.price).reduce((x, a) => x + a, 0)
   }
 
 }
