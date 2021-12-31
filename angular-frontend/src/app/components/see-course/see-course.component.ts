b157f9ed990607702c2650b6a897cb64b136140f
SimeonHristov99 <s.e.hristov99@gmail.com>
2021-12-31 17:48:40 +0200

    Add see-course component.

---
+++ angular-frontend/src/app/components/see-course/see-course.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-see-course',
+  templateUrl: './see-course.component.html',
+  styleUrls: ['./see-course.component.scss']
+})
+export class SeeCourseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
