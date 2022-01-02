6fe8b607d685a0f2c652dc9b54c2d16d62170ec9
Desislava Doncheva <43968785+dessydonchewa99@users.noreply.github.com>
2022-01-02 20:08:57 +0200

    fix creating cource

--- angular-frontend/src/app/components/add-course/add-course.component.ts
+++ angular-frontend/src/app/components/add-course/add-course.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { CourseService } from 'src/app/services/course.service';
 import { Course } from 'src/app/shared/course.model';
 
@@ -11,7 +12,7 @@ export class AddCourseComponent implements OnInit {
 
   courseBody: Course;
 
-  constructor(private courseService: CourseService) {
+  constructor(private courseService: CourseService, private router: Router) {
     this.courseBody = {
       _id: '',
       rating: 0,
@@ -28,7 +29,17 @@ export class AddCourseComponent implements OnInit {
   }
 
   addCourse() {
-    this.courseService.addCourse(this.courseBody).subscribe(res => {
+    let courseBody: any = {
+      rating: this.courseBody.rating,
+      title: this.courseBody.title,
+      description: this.courseBody.description,
+      organization: this.courseBody.organization,
+      level: this.courseBody.level,
+      price: this.courseBody.price,
+      duration: this.courseBody.duration
+    }
+    this.courseService.addCourse(courseBody).subscribe(res => {
+      this.router.navigateByUrl('/organization')
     });
   }
 
