eadf7cb8e118f601f18ada604ce90b4726792c92
Desislava Doncheva <43968785+dessydonchewa99@users.noreply.github.com>
2022-01-02 19:38:40 +0200

    implement edit/delete course

--- angular-frontend/src/app/services/course.service.ts
+++ angular-frontend/src/app/services/course.service.ts
@@ -8,10 +8,45 @@ import { WebRequestService } from './web-request.service';
 })
 export class CourseService {
 
-  courses: Course[]
+  courses: Course[] = [];
 
   constructor(private webService: WebRequestService) {
     this.courses = []
+    // this.courses = [
+    //   {
+    //     id: '1',
+    //     rating: 3,
+    //     title: 'AI For Everyone 1',
+    //     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, asperiores? Veritatis eos reiciendis quis enim iste quos distinctio aliquid perspiciatis, et eveniet, expedita alias consectetur perferendis, quaerat nesciunt blanditiis amet.',
+    //     organization: 'DeepLearningAI.com',
+    //     level: 'Beginner',
+    //     url: 'https://www.youtube.com/watch?v=NWONeJKn6kc',
+    //     price: 700,
+    //     duration: 5
+    //   },
+    //   {
+    //     id: '2',
+    //     rating: 4,
+    //     title: 'AI For Everyone 2',
+    //     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, asperiores? Veritatis eos reiciendis quis enim iste quos distinctio aliquid perspiciatis, et eveniet, expedita alias consectetur perferendis, quaerat nesciunt blanditiis amet.',
+    //     organization: 'DeepLearningAI.com',
+    //     level: 'Intermediate',
+    //     url: 'https://www.youtube.com/watch?v=NWONeJKn6kc',
+    //     price: 750,
+    //     duration: 6
+    //   },
+    //   {
+    //     id: '3',
+    //     rating: 5,
+    //     title: 'AI For Everyone 3',
+    //     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, asperiores? Veritatis eos reiciendis quis enim iste quos distinctio aliquid perspiciatis, et eveniet, expedita alias consectetur perferendis, quaerat nesciunt blanditiis amet.',
+    //     organization: 'DeepLearningAI.com',
+    //     level: 'Advanced',
+    //     url: 'https://www.youtube.com/watch?v=NWONeJKn6kc',
+    //     price: 1000,
+    //     duration: 7
+    //   },
+    // ]
   }
 
   getCourses(): Observable<Course[]> {
@@ -21,24 +56,21 @@ export class CourseService {
   getCoursesByUser(): Observable<Course[]> {
     return this.webService.get('user/courses') as Observable<Course[]>;
   }
-
-  // getCourse(id: string) {
-  //   return this.webService.get('courses') as Observable<Course[]>
-  // }
-
-  addCourse(course: Course) {
-    this.courses.push(course)
+  
+  getCourse(id: string) {
+    return this.webService.get('courses') as Observable<Course[]>
   }
 
-  // updateCourse(id: string, updatedFields: Partial<Course>) {
-  //   const course = this.getCourse(id)
-  //   Object.assign(course, updatedFields)
-  // }
+  addCourse(course: Course) {
+    return this.webService.post('course', course);
+  }
 
-  deleteCourse(id: string) {
-    const courseIdx = this.courses.findIndex(c => c._id === id)
-    if (courseIdx == -1) return
-    this.courses.splice(courseIdx, 1)
+  updateCourse(course: Course) {
+    return this.webService.post('course/update', course);
+  }
+
+  deleteCourse(course: Course) {
+    return this.webService.post('course/delete', course);
   }
 
   getStudentListByCourse() {
