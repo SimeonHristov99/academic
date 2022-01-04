05ca8672d3469886abc7ef7380fc86c7fcb6efa9
Desislava Doncheva <43968785+dessydonchewa99@users.noreply.github.com>
2022-01-04 08:54:49 +0200

    reload after edit/delete course

--- angular-frontend/src/app/components/organization/organization.component.ts
+++ angular-frontend/src/app/components/organization/organization.component.ts
@@ -193,15 +193,15 @@ export class OrganizationComponent implements OnInit {
 
   editCourse(id: any): void {
     this.updateCourseBody._id = id;
-    console.log(this.updateCourseBody)
     this.courseService.updateCourse(this.updateCourseBody).subscribe(res => {
     });
+    window.location.reload();
   }
 
   deleteCourse(id: any): void {
-    console.log(this.courses[id])
     this.courseService.deleteCourse(this.courses[id]).subscribe(res => {
     });
+    window.location.reload();
   }
 
 }
